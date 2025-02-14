11b3707bb415ba59a9a18994a1e60a776f7357d3
Jasper Palfree <well.caffeinated@gmail.com>
2025-02-14 14:59:27 -0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,17 @@
 use anyhow::Result;
 
-fn init_hsm() -> Result<()> {
+fn connector() -> Result<yubihsm::Connector> {
   let connector = yubihsm::Connector::usb(&yubihsm::UsbConfig {
     serial: None,
     timeout_ms: 60000
   });
 
-  let creds = yubihsm::Credentials::default();
-  let client = yubihsm::Client::open(connector, creds, true)?;
-
-  // generate a new connection key
-  dbg!(client.device_info()?);
-
-  Ok(())
+  Ok(connector)
 }
 
 fn run_server() -> Result<()> {
   let port = 12345;
-  let connector = yubihsm::Connector::usb(&yubihsm::UsbConfig {
-    serial: None,
-    // generating RSA keys takes a while and can timeout
-    // https://github.com/iqlusioninc/yubihsm.rs/issues/267
-    timeout_ms: 60000
-  });
+  let connector = connector()?;
 
   let server = yubihsm::connector::http::Server::new(
     &yubihsm::HttpConfig {
@@ -39,10 +28,5 @@ fn run_server() -> Result<()> {
 }
 
 fn main() -> Result<()> {
-  // if we have an "init" argument, we'll initialize the HSM
-  if std::env::args().nth(1) == Some("init".into()) {
-    init_hsm()
-  } else {
-    run_server()
-  }
+  run_server()
 }
